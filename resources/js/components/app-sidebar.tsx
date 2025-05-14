bd039285cb9cf3e7acbc27dc29b62222213371c6
bALE <91659708+IqbalInsanKurnia@users.noreply.github.com>
2025-05-14 13:26:49 +0700

    adding: app-sidebar-admin

--- resources/js/components/app-sidebar.tsx
+++ resources/js/components/app-sidebar.tsx
@@ -1,36 +1,138 @@
-// import { NavFooter } from '@/components/nav-footer';
-import { NavMain } from '@/components/nav-main';
+'use client';
+
 import { NavUser } from '@/components/nav-user';
-import { Sidebar, SidebarContent, SidebarFooter, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from '@/components/ui/sidebar';
-import { type NavItem } from '@/types';
+import {
+    Sidebar,
+    SidebarContent,
+    SidebarFooter,
+    SidebarHeader,
+    SidebarMenu,
+    SidebarMenuButton,
+    SidebarMenuItem,
+    SidebarRail,
+} from '@/components/ui/sidebar';
 import { Link } from '@inertiajs/react';
-import { BookOpen, Folder, LayoutGrid } from 'lucide-react';
+import { AppWindow, ChartNoAxesColumnIncreasing, Files, House, Images, MessageCircleWarning, Users } from 'lucide-react';
+import * as React from 'react';
 import AppLogo from './app-logo';
+import { NavInfografis } from './nav-infografis';
+import { NavPenduduk } from './nav-penduduk';
+import { NavSurat } from './nav-surat';
 
-const mainNavItems: NavItem[] = [
-    {
-        title: 'Dashboard',
-        href: '/dashboard',
-        icon: LayoutGrid,
-    },
-];
+// This is sample data.
+const data = {
+    navDashboard: [
+        {
+            title: 'Dashboard',
+            url: '/dashboard',
+            icon: House,
+        },
+    ],
+    navSurat: [
+        {
+            title: 'Surat',
+            url: '#',
+            icon: Files,
+            items: [
+                {
+                    title: 'SK Pindah',
+                    url: '#',
+                },
+                {
+                    title: 'SK Domisili',
+                    url: '#',
+                },
+                {
+                    title: 'SK Kematian',
+                    url: '#',
+                },
+                {
+                    title: 'SK Usaha',
+                    url: '#',
+                },
+                {
+                    title: 'SK Tidak Mampu',
+                    url: '#',
+                },
+                {
+                    title: 'SK Kehilangan KTP',
+                    url: '#',
+                },
+                {
+                    title: 'SK Kehilangan KK',
+                    url: '#',
+                },
+                {
+                    title: 'SK Umum',
+                    url: '#',
+                },
+            ],
+        },
+    ],
+    navProfil: [
+        {
+            title: 'Profil Desa',
+            url: '#',
+            icon: AppWindow,
+        },
+    ],
+    navArtikel: [
+        {
+            title: 'Artikel Desa',
+            url: '#',
+            icon: Images,
+        },
+    ],
+    navInfografis: [
+        {
+            title: 'Infografis Desa',
+            url: '#',
+            icon: ChartNoAxesColumnIncreasing,
+            items: [
+                {
+                    title: 'Pendapatan Desa',
+                    url: '#',
+                },
+                {
+                    title: 'Belanja Desa',
+                    url: '#',
+                },
+                {
+                    title: 'Peta Fasilitas',
+                    url: '#',
+                },
+            ],
+        },
+    ],
+    navPenduduk: [
+        {
+            title: 'Penduduk',
+            url: '#',
+            icon: Users,
+            items: [
+                {
+                    title: 'Data KTP',
+                    url: '#',
+                },
+                {
+                    title: 'Data KK',
+                    url: '#',
+                },
+            ],
+        },
+    ],
+    navPengaduan: [
+        {
+            title: 'Pengaduan Warga',
+            url: '#',
+            icon: MessageCircleWarning,
+        },
+    ],
+};
 
-// const footerNavItems: NavItem[] = [
-//     {
-//         title: 'Repository',
-//         href: 'https://github.com/laravel/react-starter-kit',
-//         icon: Folder,
-//     },
-//     {
-//         title: 'Documentation',
-//         href: 'https://laravel.com/docs/starter-kits#react',
-//         icon: BookOpen,
-//     },
-// ];
-
-export function AppSidebar() {
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
     return (
-        <Sidebar collapsible="icon" variant="inset">
+        <Sidebar collapsible="icon" {...props}>
             <SidebarHeader>
                 <SidebarMenu>
                     <SidebarMenuItem>
@@ -42,15 +144,57 @@ export function AppSidebar() {
                     </SidebarMenuItem>
                 </SidebarMenu>
             </SidebarHeader>
-
             <SidebarContent>
-                <NavMain items={mainNavItems} />
+                <SidebarMenu>
+                    {data.navDashboard.map((data) => (
+                        <SidebarMenuItem key={data.title}>
+                            <SidebarMenuButton asChild>
+                                <a href={data.url}>
+                                    <data.icon />
+                                    <span>{data.title}</span>
+                                </a>
+                            </SidebarMenuButton>
+                        </SidebarMenuItem>
+                    ))}
+                    <NavSurat items={data.navSurat} />
+                    {data.navProfil.map((data) => (
+                        <SidebarMenuItem key={data.title}>
+                            <SidebarMenuButton asChild>
+                                <a href={data.url}>
+                                    <data.icon />
+                                    <span>{data.title}</span>
+                                </a>
+                            </SidebarMenuButton>
+                        </SidebarMenuItem>
+                    ))}
+                    <NavPenduduk items={data.navPenduduk} />
+                    {data.navArtikel.map((data) => (
+                        <SidebarMenuItem key={data.title}>
+                            <SidebarMenuButton asChild>
+                                <a href={data.url}>
+                                    <data.icon />
+                                    <span>{data.title}</span>
+                                </a>
+                            </SidebarMenuButton>
+                        </SidebarMenuItem>
+                    ))}
+                    <NavInfografis items={data.navInfografis} />
+                    {data.navPengaduan.map((data) => (
+                        <SidebarMenuItem key={data.title}>
+                            <SidebarMenuButton asChild>
+                                <a href={data.url}>
+                                    <data.icon />
+                                    <span>{data.title}</span>
+                                </a>
+                            </SidebarMenuButton>
+                        </SidebarMenuItem>
+                    ))}
+                </SidebarMenu>
             </SidebarContent>
-
             <SidebarFooter>
-                {/* <NavFooter items={footerNavItems} className="mt-auto" /> */}
                 <NavUser />
             </SidebarFooter>
+            <SidebarRail />
         </Sidebar>
     );
 }
